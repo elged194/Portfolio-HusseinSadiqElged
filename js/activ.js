f7b25532a8554cfbc79320bd86b056fc3db20529
Hussein Sadiq Elged <113722584+elged194@users.noreply.github.com>
2024-01-31 23:27:37 +0200

    Update activ.js

--- js/activ.js
+++ js/activ.js
@@ -1,16 +1,16 @@
 "use strict";
 /* -------------- preloader ------------- */
-// $(document).ready(function () {
-//   // Add 'no-scroll' class to body
-//   $("body").addClass("no-scroll");
-//   // Delay the preloader and hide it
-//   $(".preloader")
-//     .delay(5000)
-//     .fadeOut("slow", function () {
-//       // Remove the 'no-scroll' class from the body
-//       $("body").removeClass("no-scroll");
-//     });
-// });
+$(document).ready(function () {
+  // Add 'no-scroll' class to body
+  $("body").addClass("no-scroll");
+  // Delay the preloader and hide it
+  $(".preloader")
+    .delay(5000)
+    .fadeOut("slow", function () {
+      // Remove the 'no-scroll' class from the body
+      $("body").removeClass("no-scroll");
+    });
+});
 /* -------------- preloader ------------- */
 
 /* -------------- scroll-up ------------- */
